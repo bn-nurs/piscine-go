bd198d731c9698758d61349964fe3d6c60a497da
Your Name <you@example.com>
2019-10-16 23:20:41 +0600

    pc

--- printcomb.go
+++ printcomb.go
@@ -8,19 +8,19 @@ func PrintComb() {
 		for n := '1'; n <= '9'; n++ {
 			for v := '2'; v <= '9'; v++ {
 				if b < n && n < v {
-					if b == '7' && n =='8' && v == '9' {
+					if b == '7' && n == '8' && v == '9' {
 						z01.PrintRune(b)
 						z01.PrintRune(n)
 						z01.PrintRune(v)
-					}else{
+					} else {
 						z01.PrintRune(b)
 						z01.PrintRune(n)
 						z01.PrintRune(v)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 						
-					}	
-				}
+						}
+					}
 				}
 			}
 		}
