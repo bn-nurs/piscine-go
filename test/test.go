5abc5aa9b517138769d684dfb95148c4c0bd5f4d
Your Name <you@example.com>
2019-10-21 23:08:55 +0600

    isprime

--- test/test.go
+++ test/test.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-        "fmt"
-        piscine ".."
+	"fmt"
+	piscine ".."
 )
 
 func main() {
-	arg1 := 4
-	arg2 := 3
-	fmt.Println(piscine.Sqrt(arg1))
-	fmt.Println(piscine.Sqrt(arg2))
-
+	fmt.Println(piscine.IsPrime(5))
+	fmt.Println(piscine.IsPrime(4))
 }
