60f1115c3630c04877946d7955fd594900b9df96
Your Name <you@example.com>
2019-10-21 20:32:30 +0600

    awrt

--- strrev.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func StrRev(s string) string {
-	var nur string
-	for _, g := range s {
-		nur = string(g) + nur
-	}
-	return nur
-}
--- swap.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func Swap(a *int, b *int) {
-	o := *a
-	p := *b
-
-	*a = p
-	*b = o
-}
