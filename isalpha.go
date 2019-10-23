6d0fb7a25851d7bafc68d70dfe7a26bcc7cac37f
Your Name <you@example.com>
2019-10-23 21:05:04 +0600

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -3,8 +3,8 @@ package piscine
 func IsAlpha(str string) bool {
 	for _, i := range str {
 		if !(i >= 'A' && i <= 'Z' || i >= 'a' && i <= 'z' || i >= '0' && i <= '9') {
-			return false 
+			return false
 		}
-	} 
+	}
 	return true
 }
