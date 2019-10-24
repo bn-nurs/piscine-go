f03cf6edcbd6372ae8f064a6344f7badf7ef78e7
Your Name <you@example.com>
2019-10-24 18:43:14 +0600

    revparams

--- revparams/main.go
+++ revparams/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	arguments := os.Args
+	arg := os.Args
 	k := 0
-	for i := range arguments {
+	for i := range arg {
 		i = i
 		k++
 	}
 	if i := k - 1; i > 0; i-- {
-		str := arguments[i]
+		str := arg[i]
 		slice := []rune(str)
 		for _, letter := range slice {
 			z01.PrintRune(letter)
 		}
-		z01.PrintRune(10)
+		z01.PrintRune('\n')
 	}
 
 }
